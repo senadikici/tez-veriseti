81cb76b5879b6415a9d3c211dc9adbe2748eabf0
Sena <48378319+senadikici@users.noreply.github.com>
2023-12-16 21:45:10 +0300

    Add files via upload

---
+++ test-eksik-veriseti/karisik2.py
@@ -0,0 +1,7 @@
+print("deneme")
+def sayilari_listele(baslangic,bitis):
+    for sayi in range(baslangic,bitis):
+        print(sayi)
+sayilari_listele1,10)
+
+print("deneme")
---
+++ test-eksik-veriseti/testwhile45.py
@@ -0,0 +1,11 @@
+print("deneme")
+number = int(input("deneme"))
+divisor = 2
+
+number > 1:
+    if number % divisor == 0:
+        print(divisor)
+        number //= divisor
+    else:
+        divisor += 1
+print("deneme")
