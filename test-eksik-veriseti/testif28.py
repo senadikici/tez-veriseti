8977cc36c6aca86184e844b771b4315b442fc911
Sena <48378319+senadikici@users.noreply.github.com>
2023-12-16 21:40:52 +0300

    Add files via upload

---
+++ test-eksik-veriseti/testif28.py
@@ -0,0 +1,8 @@
+print("deneme")
+number = int(input("deneme"))
+
+number % 2 == 0:
+    print("deneme")
+else:
+    print("deneme")
+print("deneme")
---
+++ test-eksik-veriseti/testif37.py
@@ -0,0 +1,9 @@
+print("deneme")
+age = int(input("deneme"))
+if age < 18:
+    print("deneme")
+elage < 30:
+    print("deneme")
+else:
+    print("deneme")
+print("deneme")
